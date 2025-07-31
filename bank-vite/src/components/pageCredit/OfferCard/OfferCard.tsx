0761405c93d90ced92960315b9fc359f7568c544
Pavel <pavel.khvostov@inbox.ru>
2025-07-31 04:07:21 +0300

    update offers style

--- bank-vite/src/components/pageCredit/OfferCard/OfferCard.tsx
+++ bank-vite/src/components/pageCredit/OfferCard/OfferCard.tsx
@@ -25,41 +25,43 @@ interface IOfferCardProps {
 const OfferCard: React.FC<IOfferCardProps> = ({ offer, onSelect }) => {
   return (
     <div className='offer-card'>
-      <img
-        className='offer-card__img'
-        src={offerImg}
-        width={150}
-        height={150}
-        alt='коробка с конфетти'
-      />
-      <div className='offer-card__wrap'>
-        <span className='offer-card__text'>
-          Requested amount: {offer.requestedAmount.toLocaleString()}&nbsp;₽
-        </span>
-        <span className='offer-card__text'>
-          Total amount: {offer.totalAmount.toLocaleString()}&nbsp;₽
-        </span>
-        <span className='offer-card__text'>For {offer.term} months</span>
-        <span className='offer-card__text'>
-          Monthly payment:{' '}
-          {offer.monthlyPayment.toLocaleString(undefined, { maximumFractionDigits: 2 })}&nbsp;₽
-        </span>
-        <span className='offer-card__text'>Your rate: {offer.rate}%</span>
-        <span className='offer-card__text'>
-          {offer.isInsuranceEnabled ? 'Insurance included' : 'No insurance'}
-          <img
-            src={offer.isInsuranceEnabled ? checkImg : closeImg}
-            alt='картика галочки или крестика'
-          />
-        </span>
-        <span className='offer-card__text'>
-          {offer.isSalaryClient ? 'Salary client' : 'Not a salary client'}
-          <img
-            src={offer.isSalaryClient ? checkImg : closeImg}
-            alt='картика галочки или крестика'
-          />
-        </span>
+      <div className='offer-card__inner'>
+        <img
+          className='offer-card__img'
+          src={offerImg}
+          width={150}
+          height={150}
+          alt='коробка с конфетти'
+        />
+        <div className='offer-card__wrap'>
+          <span className='offer-card__text'>
+            Requested amount: {offer.requestedAmount.toLocaleString()}&nbsp;₽
+          </span>
+          <span className='offer-card__text'>
+            Total amount: {Math.round(offer.totalAmount).toLocaleString()}&nbsp;₽
+          </span>
+          <span className='offer-card__text'>For {offer.term} months</span>
+          <span className='offer-card__text'>
+            Monthly payment: {Math.round(offer.monthlyPayment).toLocaleString()}&nbsp;₽
+          </span>
+          <span className='offer-card__text'>Your rate: {offer.rate}%</span>
+          <span className='offer-card__text'>
+            Insurance included
+            <img
+              src={offer.isInsuranceEnabled ? checkImg : closeImg}
+              alt='картика галочки или крестика'
+            />
+          </span>
+          <span className='offer-card__text'>
+            Salary client
+            <img
+              src={offer.isSalaryClient ? checkImg : closeImg}
+              alt='картика галочки или крестика'
+            />
+          </span>
+        </div>
       </div>
+
       <Button
         className='offer-card__btn'
         children='Select'
